4238baabc138838bd1f2992e83e468eb94bad82c
MrPOS666 <ruitaozou@gmail.com>
2024-09-16 18:54:59 -0400

    Done TODO in MultiSet.java

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -11,4 +11,9 @@ public abstract class MultiSet {
     abstract void add(Integer item);
 
     // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
+    public void remove(int x) {}
+    public boolean isEmpty() { return false; }
+    public int count(String a, String b) { return 0; }
+    public int size() { return 0; }
+    public static void main(String[] args) {}
 }
